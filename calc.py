1f853fcc1f089c618fae0b111a15a02a996df78f
Princebhai Makwana <princemakwana649@gmail.com>
2025-08-22 22:06:59 +0530

    Subtraction

--- calc.py
+++ calc.py
@@ -4,3 +4,6 @@ num2 = float(input("Enter second number: "))
 addition = num1 + num2
 print("\nResults:")
 print(f"Addition: {num1} + {num2} = {addition}")
+
+subtraction = num1 - num2
+print(f"Subtraction: {num1} - {num2} = {subtraction}")
