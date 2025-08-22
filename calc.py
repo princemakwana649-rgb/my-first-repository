03dca4cad4a2f89637c97c01440b0de6987bc8ed
Princebhai Makwana <princemakwana649@gmail.com>
2025-08-22 22:05:25 +0530

    Addition

--- calc.py
+++ calc.py
@@ -1,2 +1,6 @@
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
+
+addition = num1 + num2
+print("\nResults:")
+print(f"Addition: {num1} + {num2} = {addition}")
