6e41b1aaadf88f1526e5e667f10261500a0bfe58
Princebhai Makwana <princemakwana649@gmail.com>
2025-08-22 22:08:00 +0530

    Multiplication

--- calc.py
+++ calc.py
@@ -7,3 +7,7 @@ print(f"Addition: {num1} + {num2} = {addition}")
 
 subtraction = num1 - num2
 print(f"Subtraction: {num1} - {num2} = {subtraction}")
+
+multiplication = num1 * num2
+print(f"Multiplication: {num1} * {num2} = {multiplication}")
+
